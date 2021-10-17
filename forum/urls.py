966bc0bff916d44a127d610c4b839fa72cfcbd68
polarity-cf <futanaristic@gmail.com>
2021-10-18 02:56:08 +0700

    somehow made the thread work

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.urls import path
-from .views import CreateThread, TestingView, ThreadList
+from .views import *
 urlpatterns = [
     path('', ThreadList.as_view(), name="home"),
     path('create-thread/', CreateThread.as_view(), name="create_thread"),
+    path('thread/<int:pk>/', HandlingThread, name="thread")
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,15 @@
+from django.db.models.base import Model
 from django.shortcuts import render
 from django.urls.base import reverse_lazy
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.forms import ModelForm
+from django import forms
+from django.views.generic import View
+import datetime
 
 from forum.models import Thread, Comment
-from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.views.generic.edit import CreateView, DeleteView, FormView
 # Create your views here.
 
 from django.http import HttpResponse
@@ -15,9 +21,46 @@ class ThreadList(ListView):
     model = Thread
     context_object_name = 'threads'
 
-
 class CreateThread(CreateView):
     model = Thread
     fields = ['creator', 'title', 'description']
     success_url = reverse_lazy('home')
+
+def HandlingThread(request, pk):
+    thread = Thread.objects.get(id=pk)
+
+    if request.method == 'POST':
+        creator = request.POST.get('creator')
+        message = request.POST.get('message')
+        Comment.objects.create(thread=thread, creator=creator, message=message)
+
+        thread.updated = datetime.datetime.now().time()
+        thread.save()
+
+    comments = Comment.objects.filter(thread=thread)
+
+    last_update = thread.updated
+
+    context = {
+        'comments': comments,
+        'last_update': last_update
+    }
+
+    return render(request, 'forum/thread.html', context)
+    
+    
+
+
+    
+    
+    
+    
+
+
+
+
+    
+
+
+
 
