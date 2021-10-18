264cf2423429bfc233837d1ab80bb523897ce9d1
polarity-cf <futanaristic@gmail.com>
2021-10-18 17:54:31 +0700

    minor fix

--- forum/urls.py
+++ forum/urls.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.urls import path
 from .views import *
+
 urlpatterns = [
     path('', ThreadList.as_view(), name="home"),
     path('create-thread/', CreateThread.as_view(), name="create_thread"),
